f671102d82bba6c550459d3ecfd76305c8f10a46
fabienhoti <fabien.hoti.yahoo@icloud.com>
2022-03-08 19:07:20 +0100

    user info file update

--- user_info.py
+++ user_info.py
@@ -4,7 +4,6 @@ import json
 
 
 
-
 def getClient():
     client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
                            #consumer_key=config.API_KEY,
@@ -15,24 +14,18 @@ def getClient():
     return client
 
 
-
-
 def get_user_information(username):
     client = getClient()
-    user = client.get_users(usernames=["FabienHoti"])
-    user_info=[]
+    user = client.get_users(usernames=username)
 
-    for x in user:
-        dic = {}
-        dic['id']=x.id
-        dic['username']=x.usernames
-        user_info.append(dic)
-    return user_info
+    return user
 
 
 
 #---------------- aufrufen von user info die man eingibt
-#user_info = get_user_information("FabienHoti")
+user_info = get_user_information("FabienHoti")
+print(user_info.data)
+
 
 
 
