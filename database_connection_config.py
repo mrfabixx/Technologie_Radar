c78e89f7fd1d2f5123489e4b314548468dd879c5
Criwi66 <100606813+Criwi66@users.noreply.github.com>
2022-05-26 22:31:20 +0200

    Update database_connection_config.py

--- database_connection_config.py
+++ database_connection_config.py
@@ -2,16 +2,20 @@ from configparser import ConfigParser
 
 
 def config(filename='dbcon.ini', section='postgresql'):
+    # create a parser
     parser = ConfigParser()
     parser.read(filename)
 
+    #get section, default (psql)
     db = {}
-
+    #check to see if section (psql) parser exists
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
             db[param[0]] = param[1]
-
+    
+    #return an error if param is called that is NOT listed in (init) file
+    
     else:
         raise Exception('Section{0} not fount in the file {1}'.format(section, filename))
 
