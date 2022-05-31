02d770d1f6fc8e0d0800a4ebc69e7da41d2b9915
domehnt <100583002+domehnt@users.noreply.github.com>
2022-05-31 17:04:15 +0200

    Update hand_over_results.py

--- hand_over_results.py
+++ hand_over_results.py
@@ -1,6 +1,6 @@
-import psycopg2
+import psycopg2 #Python driver for PostgreSQL
 
-from database_connection_config import config
+from database_connection_config import config   #config file for log into database.
 
 #creates a list wich contains the Sentiment result for each tweet 
 def get_digitsentimentresults():
@@ -14,12 +14,12 @@ def get_digitsentimentresults():
     cur.execute("select sentiment from sentimentresults order by sentiment;")
 
     while True:
-        try:
+        try:    #Exception handling, if try cannot run --> Error caught below.
             row = cur.fetchone()
             data = float(row[0])
-            resultDigits.append(data)
+            resultDigits.append(data)   #List of floats is added to the 'data' list. Important for diagram.py file, because of the import!
 
-        except TypeError:
+        except TypeError:   # If try does not run the error is caught here!
             break
     cur.close()
     conn.close()
